54e4640d9e9997bc14acefcaca9a1dd687051395
David Bwire <israelbwire@gmail.com>
2016-09-12 14:54:01 +0300

    adjust initializer to be zf2 and zf3 compatible

--- src/Helper/ServiceManager/TableGatewayInitializer.php
+++ src/Helper/ServiceManager/TableGatewayInitializer.php
@@ -6,9 +6,10 @@
 
 namespace Helper\ServiceManager;
 
-use Zend\ServiceManager\Initializer\InitializerInterface;
 use Interop\Container\ContainerInterface;
 use Helper\Mapper\TableGateway;
+use Zend\ServiceManager\InitializerInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Description of TableGatewayInitializer
@@ -18,12 +19,35 @@ use Helper\Mapper\TableGateway;
 class TableGatewayInitializer implements InitializerInterface
 {
 
+    /**
+     * zf3
+     * 
+     * @param ContainerInterface $container
+     * @param TableGateway $instance
+     * @return type
+     */
     public function __invoke(ContainerInterface $container, $instance)
     {
         if (!$instance instanceof TableGateway) {
             return;
         }
         $instance->setLogger($container->get('logger'));
+    }
+
+    /**
+     * zf2
+     * 
+     * @param TableGateway $instance
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return void
+     */
+    public function initialize($instance,
+            ServiceLocatorInterface $serviceLocator)
+    {
+        if (!$instance instanceof TableGateway) {
+            return;
+        }
+        $instance->setLogger($serviceLocator->get('logger'));
     }
 
 }
